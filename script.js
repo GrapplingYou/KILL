a5fd4a19b71da0a4724a2c31964afe38529205c4
GrapplingYou <77642984+GrapplingYou@users.noreply.github.com>
2021-02-09 21:50:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var btn1 = document.getElementById("btn1")
+
+var text1 = document.getElementById("body")
+
+btn1.addEventListener("click",fun1)
+function fun1 (){   
+    text1.innerHTML = 'Nikomek L9a7ba'
+}
+
+
+
+
+
+
+
+
